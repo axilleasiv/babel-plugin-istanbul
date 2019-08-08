44505564c1044b0e3771984d0a37712f98a174f4
Achilleas Kyritsakas <akiritsakas@gmail.com>
2019-08-08 12:36:50 +0300

    feat: add included assets

--- src/index.js
+++ src/index.js
@@ -1,29 +1,46 @@
-const { basename } = require('path');
+'use strict';
+
+const { relative, dirname } = require('path');
 const t = require('@babel/types');
 const { programVisitor } = require('@achil/istanbul-lib-instrument');
+
+function isReplFile(state) {
+  var dir = dirname(state.opts.fileName);
+  var rel = relative(dir, state.file.opts.filename);
+
+  if (state.opts.included.indexOf(rel) !== -1) {
+    state.rel = rel;
+    return true;
+  }
+
+  return false;
+}
 
 module.exports = function() {
   return {
     visitor: {
       Program: {
         enter(path, state) {
-          if (state.file.opts.filename !== state.opts.fileName) {
+          if (!isReplFile(state)) {
+            path.skip();
             return;
           }
-          const name = basename(state.opts.fileName);
 
           this.__dv__ = null;
-          this.__dv__ = programVisitor(t, name, {
+          this.__dv__ = programVisitor(t, state.rel, {
             coverageVariable: state.opts.cvVar,
             cvIncreaseCb: state.opts.cvIncreaseCb,
             inputSourceMap: undefined
           });
+
           this.__dv__.enter(path);
         },
+
         exit(path) {
           if (!this.__dv__) {
             return;
           }
+
           this.__dv__.exit(path);
         }
       }
