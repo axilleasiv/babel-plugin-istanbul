8e991265d42facfe17e5042710407494c3961b5c
Achilleas Kyritsakas <akiritsakas@gmail.com>
2019-08-18 00:38:32 +0300

    fix: remove included from options

--- src/index.js
+++ src/index.js
@@ -1,33 +1,16 @@
 'use strict';
 
-const { relative, dirname } = require('path');
 const t = require('@babel/types');
+
 const { programVisitor } = require('@achil/istanbul-lib-instrument');
-
-function isReplFile(state) {
-  var dir = dirname(state.opts.fileName);
-  var rel = relative(dir, state.file.opts.filename);
-
-  if (state.opts.included.indexOf(rel) !== -1) {
-    state.rel = rel;
-    return true;
-  }
-
-  return false;
-}
 
 module.exports = function() {
   return {
     visitor: {
       Program: {
         enter(path, state) {
-          if (!isReplFile(state)) {
-            path.skip();
-            return;
-          }
-
           this.__dv__ = null;
-          this.__dv__ = programVisitor(t, state.rel, {
+          this.__dv__ = programVisitor(t, state.opts.rel, {
             coverageVariable: state.opts.cvVar,
             cvIncreaseCb: state.opts.cvIncreaseCb,
             inputSourceMap: undefined
